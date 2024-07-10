c316db5f8de79f45a5cd70f4e8fb6fc4ccf8e635
Paing Visal <108209001+PaingVisal@users.noreply.github.com>
2024-07-11 02:35:57 +0700

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'fashion by sal24k'
+
+version '1.2.0'
+
+client_scripts {
+  "config.lua",
+  "client/client.lua"
+}
+
+server_scripts {
+  "config.lua",
+  "server/server.lua"
+}
+
+files {
+    "stream/prop_bagcat_chill.ydr",
+    "stream/prop_newplay.ydr",
+    "stream/noone_cuteghosts_anim_fashions.ydr",
+    "stream/noone_cuteghosts_anim_fashions.ycd",
+}	
+--dont forget replace in resource
+data_file "DLC_ITYP_REQUEST" "stream/prop_bagcat_chill.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/prop_nplay.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/noone_cuteghosts_anim_fashions.ytyp"
---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config['Event'] = {
+    [1] = "qb-core:client:setup",
+    [2] = "qb-inventory:client:closeInventory",
+}
+--
+Config.itemuse1 = 'prop_bagcat_chill'
+Config.prop1 = 'prop_bagcat_chill'
+
+Config.itemuse2 = 'noone_cuteghosts_anim_fashions'
+Config.prop2 = 'noone_cuteghosts_anim_fashions'
